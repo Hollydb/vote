262383894cfd051ff1742b36bd857dcc054b592d
Holly DeBelser and Mike Lofgren <hd:holly.debelser@gmail.com ml:mikeylofgren@gmail.com>
2019-06-06 11:18:44 -0700

    Add functionality for address finder, links and Jquery.

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,30 @@ $(document).ready(function(){
   $("form#age-form").submit(function(event){
     event.preventDefault()
     var age = parseInt($('#ageInput').val());
-  if (age>=18){
-    $('#over18').show();
-    $('#under18').hide();
-} else {
-    $('#under18').show();
-    $('#over18').hide();
-  }
-});
+    if (age>=18){
+      $('#over18').show();
+      $('#under18').hide();
+    } else {
+      $('#under18').show();
+      $('#over18').hide();
+    }
+  });
 
   $("form#address-form").submit(function(event){
     event.preventDefault()
     var address = $("#addressInput").val();
-  if (address <= 49999){
-    $().show();
-  } else if (address >=50000) {
-    $().show();
-  } else {
-    $().show();
-  }
-  }
+    if (address <= 49999){
+      $(".east").show();
+      $(".mid").hide();
+      $(".west").hide();
+    } else if (address >= 50000 && address <= 89999) {
+      $(".mid").show();
+      $(".east").hide();
+      $(".west").hide();
+    } else {
+      $(".west").show();
+      $(".east").hide();
+      $(".mid").hide();
+    }
+  });
 });
