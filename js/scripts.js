1a5eda6d9a48b469833ec3fdd7b69e1c1acbc04f
Holly DeBelser and Mike Lofgren <mike@>
2019-06-06 10:26:15 -0700

    Add form functionality and Jquery logic to page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
   console.log ("Jquery is loaded");
-  var age= parseInt(prompt("How old are you?"));
+  $("form").submit(function(event){
+    event.preventDefault()
+    var age = parseInt($('#ageInput').val());
   if (age>=18){
     $('#over18').show();
+    $('#under18').hide();
 } else {
     $('#under18').show();
+    $('#over18').hide();
   }
+});
 });
